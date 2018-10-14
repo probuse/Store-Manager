49a504f00844233c20567fe997098a6b3be31f42
myrdstom <nserekopaul@gmail.com>
2018-10-14 17:27:15 +0300

    chore: Ensuring that written respective tests pass[#161204053]

--- tests/product_tests.py
+++ tests/product_tests.py
@@ -13,21 +13,24 @@ class FlaskTestCase(unittest.TestCase):
         with self.app.test_client() as client:
             response = client.get("/v1/products",
                                   content_type="application/json",
-                                  data=json.dumps(dict(product_name="Lenovo")))
+                                  data=json.dumps(dict(product_name="Acer",
+                                                       price_per_unit="19000000",
+                                                       stock=100)))
             response_json = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 200)
-            # self.assertEqual("Lenove", response_json[0]['product'])
+            self.assertIn("Acer", response_json[0]['product_name'])
 
     """testing creation of a new question"""
 
-    def test_add_new_question(self):
+    def test_add_new_inventory_item(self):
         with self.app.test_client() as client:
-            response = client.post('/v1/questions/MakeQuestions', content_type='application/json',
-                                   data=json.dumps(dict(question="Is python growing?")))
-
+            response = client.post('/v1/products', content_type='application/json',
+                                   data=json.dumps(dict(product_name="Acer",
+                                                        price_per_unit="19000000",
+                                                        stock = 100)))
             self.assertEqual(response.status_code, 201)
             responseJson = json.loads(response.data.decode())
-            self.assertIn('question created', responseJson['message'])
+            self.assertIn('product created', responseJson['message'])
 
 
     if __name__ == '__main__':
