7b445d0a7c10c27fff8ac9598e2bd85dddd0f6c7
myrdstom <nserekopaul@gmail.com>
2018-10-20 22:19:09 +0300

    chore: Refactoring code[#161108998]

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .api import apcn_v1
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.errorhandler(404)
+    def not_found(e):
+        response = jsonify({'message': 'The requested Resource does not exist; Please review the URL'})
+        response.status_code = 404
+        return response
 
     app.register_blueprint(apcn_v1)
 
--- app/api/products.py
+++ app/api/products.py
@@ -17,88 +17,84 @@ class Products(Resource):
     """This function returns a list of all products in the inventory"""
 
     def get(self, product_id=0):
-        product_response = []
-        prod_list = []
-        if(product_id):
+        all_products = []
+        single_product = []
+        if (product_id):
             # prod_list =
             for product_list in products_list:
                 if product_list.product_id == product_id:
                     req_dict = (product_list.to_json_id())
-                    prod_list.append(req_dict)
-            if not prod_list:
+                    single_product.append(req_dict)
+            if not single_product:
                 return {'message': 'product not in inventory'}, 200
             else:
-                return prod_list, 200
+                return single_product, 200
         else:
             for product_list in products_list:
-                product_response.append(product_list.to_json())
-            if not product_response:
+                all_products.append(product_list.to_json())
+            if not all_products:
                 return {'message': 'No product in inventory'}, 200
-            return product_response, 200
+            return all_products, 200
 
     def post(self):
         """This function lets the administrator add a new product to the inventory"""
-        try:
-            data = request.get_json()
-            product_id = len(products_list) + 1
-            product_name = data['product_name']
-            unit_price = data['unit_price']
-            stock = data['stock']
-            if not isinstance(product_name, str) or not isinstance(unit_price, int) or not isinstance(stock, int):
-                return {'message': 'Error:Invalid value please review product inputs'}, 400
-            if not empty_string_catcher(product_name):
-                return {'message': 'Empty values are not allowed'}, 400
-            products_list.append(Productpoints(product_id, product_name, unit_price, stock))
-            return {'message': 'product created'}, 201
-        except:
-            return {'message': 'Please review columns added'}, 400
+
+        data = request.get_json()
+        product_id = len(products_list) + 1
+        product_name = data['product_name']
+        unit_price = data['unit_price']
+        stock = data['stock']
+        if not isinstance(product_name, str) or not isinstance(unit_price, int) or not isinstance(stock, int):
+            return {'message': 'Error:Invalid value please review product inputs'}, 400
+        if not empty_string_catcher(product_name):
+            return {'message': 'Empty values are not allowed'}, 400
+        products_list.append(Productpoints(product_id, product_name, unit_price, stock))
+        return {'message': 'product created'}, 201
 
 
 class Sales(Resource):
     """This function returns a list of all products in the inventory"""
+
     def get(self, sale_id=0):
-        sales_response = []
-        ans_list = []
+        all_sales = []
+        single_sale = []
         if (sale_id):
             for sale_list in sales_list:
                 if sale_list.sale_id == sale_id:
                     req_dict = (sale_list.to_json_id())
-                    ans_list.append(req_dict)
-            if not ans_list:
+                    single_sale.append(req_dict)
+            if not single_sale:
                 return {'message': 'product not in inventory'}, 200
             else:
-                return ans_list, 200
+                return single_sale, 200
         else:
             for sale_list in sales_list:
-                sales_response.append(sale_list.to_json())
-            return sales_response, 200
+                all_sales.append(sale_list.to_json())
+            return all_sales, 200
 
     def post(self):
         """This function lets the administrator add a new product to the inventory"""
-        try:
-            data = request.get_json()
-            sale_id = len(sales_list) + 1
-            product_id = data['product_id']
-            product_name = data['product_name']
-            unit_price = data['unit_price']
-            quantity = data['quantity']
-            total = data['quantity'] * data['unit_price']
-            if not isinstance(product_name, str) or not isinstance(product_id, int) or not isinstance(unit_price, int) \
-                    or not isinstance(quantity, int):
-                return {'message': 'Error:Invalid value for product_name'}, 400
-            if not empty_string_catcher(product_name):
-                return {'message': 'Empty values are not allowed'}, 400
-            for product_list in products_list:
-                if product_list.product_id != product_id or product_list.unit_price != unit_price \
-                        or product_list.product_name != data["product_name"]:
-                    return {'message': 'non existent product'}, 400
-                else:
-                    sales_list.append(Salepoints(sale_id, product_id, product_name, unit_price, quantity, total))
-                return {'message': 'sale made'}, 201
-        except:
-            return {'message': 'Please review columns added'}, 400
+
+        data = request.get_json()
+        sale_id = len(sales_list) + 1
+        product_id = data['product_id']
+        product_name = data['product_name']
+        unit_price = data['unit_price']
+        quantity = data['quantity']
+        total = data['quantity'] * data['unit_price']
+        if not isinstance(product_name, str) or not isinstance(product_id, int) or not isinstance(unit_price, int) \
+                or not isinstance(quantity, int):
+            return {'message': 'Error:Invalid value added, please review'}, 400
+        if not empty_string_catcher(product_name):
+            return {'message': 'Empty values are not allowed'}, 400
+        for product_list in products_list:
+            if product_list.product_id != product_id or product_list.unit_price != unit_price \
+                    or product_list.product_name != data["product_name"]:
+                return {'message': 'non existent product'}, 400
+            else:
+                sales_list.append(Salepoints(sale_id, product_id, product_name, unit_price, quantity, total))
+            return {'message': 'sale made'}, 201
 
 
 API.add_resource(Products, '/products', '/products/<int:product_id>')
 API.add_resource(Sales, '/sales', '/sales/<int:sale_id>')
-
