29621f4ec1e8992bebc9fd8094c77fb8fe52aa8f
myrdstom <nserekopaul@gmail.com>
2018-10-24 17:58:18 +0300

    chore: Roling back changes that are breaking code [#161451461]

--- app/api/products.py
+++ app/api/products.py
@@ -17,49 +17,39 @@ API = Api(apcn_v1)
 class Products(Resource):
     """This function returns a list of all products in the inventory"""
 
-    @jwt_required
     def get(self, product_id=0):
-        current_user = get_jwt_identity()
-        if current_user:
-            all_products = []
-            single_product = []
-            if (product_id):
-                for product_list in products_list:
-                    if product_list.product_id == product_id:
-                        req_dict = (product_list.to_json_id())
-                        single_product.append(req_dict)
-                if not single_product:
-                    return {'message': 'product not in inventory'}, 200
-                else:
-                    return single_product, 200
+        all_products = []
+        single_product = []
+        if (product_id):
+            for product_list in products_list:
+                if product_list.product_id == product_id:
+                    req_dict = (product_list.to_json_id())
+                    single_product.append(req_dict)
+            if not single_product:
+                return {'message': 'product not in inventory'}, 200
             else:
-                for product_list in products_list:
-                    all_products.append(product_list.to_json())
-                if not all_products:
-                    return {'message': 'No product in inventory'}, 200
-                return all_products, 200
+                return single_product, 200
         else:
-            return {'message': 'Please sign up to access resource'}, 409
+            for product_list in products_list:
+                all_products.append(product_list.to_json())
+            if not all_products:
+                return {'message': 'No product in inventory'}, 200
+            return all_products, 200
 
     @jwt_required
     def post(self):
         """This function lets the administrator add a new product to the inventory"""
-        current_user = get_jwt_identity()
-        logged_user = current_user['is_owner']
-        if logged_user:
-            data = request.get_json()
-            product_id = len(products_list) + 1
-            product_name = data['product_name']
-            unit_price = data['unit_price']
-            stock = data['stock']
-            if not isinstance(product_name, str) or not isinstance(unit_price, int) or not isinstance(stock, int):
-                return {'message': 'Error:Invalid value please review product inputs'}, 400
-            if not empty_string_catcher(product_name):
-                return {'message': 'Empty values are not allowed'}, 400
-            products_list.append(Productpoints(product_id, product_name, unit_price, stock))
-            return {'message': 'product created'}, 201
-        else:
-            return {'message': 'you are not authorized to view this resource'}, 401
+        data = request.get_json()
+        product_id = len(products_list) + 1
+        product_name = data['product_name']
+        unit_price = data['unit_price']
+        stock = data['stock']
+        if not isinstance(product_name, str) or not isinstance(unit_price, int) or not isinstance(stock, int):
+            return {'message': 'Error:Invalid value please review product inputs'}, 400
+        if not empty_string_catcher(product_name):
+            return {'message': 'Empty values are not allowed'}, 400
+        products_list.append(Productpoints(product_id, product_name, unit_price, stock))
+        return {'message': 'product created'}, 201
 
 
 class Sales(Resource):
@@ -103,5 +93,5 @@ class Sales(Resource):
             return {'message': 'sale made'}, 201
 
 
-API.add_resource(Products, '/products', '/products/<int:product_id>')
+API.add_resource(Products, '/products/', '/products/<int:product_id>')
 API.add_resource(Sales, '/sales', '/sales/<int:sale_id>')
